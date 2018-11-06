902793f5ff049fa92adc31013daa101b7b02ef8a
yusuke shimabukuro <e185723@ie.u-ryukyu.ac.jp>
2018-11-06 09:17:28 +0900

    prog2 week6

---
+++ src/test/SampleTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Asserti
+
+class SampleTest {
+
+    @Test
+    void add() {
+        Sample sample = new Sample();
+        int result = sample.add(a:2,b:3);
+        assertEquals(expe)
+    }
+}
